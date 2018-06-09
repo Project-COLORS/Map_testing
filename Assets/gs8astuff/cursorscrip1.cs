ea8508390363bfa3149d4ce41c82846dd5e6ec45
risonakamo <hmykanae@gmail.com>
2018-06-08 20:44:53 -0400

    rotation interpolation

--- Assets/gs8astuff/cursorscrip1.cs
+++ Assets/gs8astuff/cursorscrip1.cs
@@ -20,18 +20,22 @@ public class cursorscrip1:MonoBehaviour
     float m_cursorspeed=7.6f;
 
     Vector3 m_camAngle=new Vector3(30,0,0);
-    /*
-        for each camera position (4 positions):
+    /*  for each camera position (4 positions):
         0: multiplier for X position shift
         1: multiplier for Z position shift
-        2: Y angle of camera
+        2: Y angle of camera (currently not being used)
         3: horizontal control cursor movement axis. 0 for X, 2 for z
         4: vertical control cursor movement axis
         5: horizontal control invert. 1 for normal, -1 for inverted
-        6: vertical control invert
-    */
-    int [,] m_camPositions=new int[4,7]{{1,1,225,0,2,-1,1},{-1,1,135,2,0,-1,-1},{-1,-1,45,0,2,1,-1},{1,-1,315,2,0,1,1}};
+        6: vertical control invert*/
+    int [,] m_camPositions=new int[4,7]{{1,1,225,0,2,-1,1},{-1,1,135,2,0,-1,-1},{-1,-1,45,0,2,1,-1},{1,-1,-45,2,0,1,1}};
+    //index number of camPosition array for current position
     int m_currentcamPosition=0;
+    //{x position,z position,y degree} current values for
+    //cam transforms, for intermediate interpolation
+    float[] m_camPositionsCurrent=new float[3]{0,0,0};
+    //camera angle to interpolate to. set on rotation button presses
+    float m_targetcamYAngle=225f;
 
     /*-- grid float vars, might deprecate later --*/
     [NonSerialized]
@@ -79,6 +83,7 @@ public class cursorscrip1:MonoBehaviour
 
         if (Input.GetButtonDown("rotateleft"))
         {
+            m_targetcamYAngle-=90;
             if (m_currentcamPosition>=3)
             {
                 m_currentcamPosition=0;
@@ -92,6 +97,7 @@ public class cursorscrip1:MonoBehaviour
 
         else if (Input.GetButtonDown("rotateright"))
         {
+            m_targetcamYAngle+=90;
             if (m_currentcamPosition<=0)
             {
                 m_currentcamPosition=3;
@@ -112,12 +118,16 @@ public class cursorscrip1:MonoBehaviour
         // transform.Translate(m_moveVec,Space.World);
         m_body.velocity=t_moveVec;
 
-        m_posvec=transform.position;
-        m_posvec.x+=5*m_camPositions[m_currentcamPosition,0];
-        m_posvec.y+=4;
-        m_posvec.z+=5*m_camPositions[m_currentcamPosition,1];
+        m_camPositionsCurrent[0]=Mathf.Lerp(m_camPositionsCurrent[0],5*m_camPositions[m_currentcamPosition,0],.1f);
+        m_camPositionsCurrent[1]=Mathf.Lerp(m_camPositionsCurrent[1],5*m_camPositions[m_currentcamPosition,1],.1f);
+        m_camPositionsCurrent[2]=Mathf.Lerp(m_camPositionsCurrent[2],m_targetcamYAngle,.1f);
 
-        m_camAngle.y=m_camPositions[m_currentcamPosition,2];
+        m_posvec=transform.position;
+        m_posvec.x+=m_camPositionsCurrent[0];
+        m_posvec.y+=4;
+        m_posvec.z+=m_camPositionsCurrent[1];
+
+        m_camAngle.y=m_camPositionsCurrent[2];
 
         m_cam.transform.eulerAngles=m_camAngle;
         m_cam.transform.position=m_posvec;
