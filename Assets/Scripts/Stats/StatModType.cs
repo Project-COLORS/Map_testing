80e21f133d1b1d6933db0b97951c139d3c398753
trayemo <trayemo@gmail.com>
2018-07-03 00:56:00 -0400

    finally finished the stat system and added a script folder to the unity project for organization

---
+++ Assets/Scripts/Stats/StatModType.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public enum StatModType // allows percentages and flat values to be calculated for stat modifiers
+{
+    Flat = 100,
+    PercentAdd = 200, //add percentages
+    PercentMult = 300,
+    //initializing these three variables allows for the order of statmodtype to be customizable. for instance if there was need for a flat modifier to be applied between percentAdd and percent Mult we can assign it an Order number between 201-299
+}
---
+++ Assets/Scripts/character/basechar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basechar:MonoBehaviour
+{
+    cursorscrip1 m_cursor;
+    tilecontrol m_tilecontrol;
+
+    public int[] m_pos=new int[2];
+
+    void Start()
+    {
+        m_cursor=GameObject.Find("cursor").GetComponent<cursorscrip1>();
+        m_tilecontrol=GameObject.Find("tilesystem").GetComponent<tilecontrol>();
+
+        move();
+    }
+
+    void move()
+    {
+        m_cursor.enterSelectMode(relocate);
+    }
+
+    void relocate()
+    {
+        Vector3 newpos=new Vector3(m_cursor.m_selectedTile.m_centre[0],.64f,m_cursor.m_selectedTile.m_centre[1]);
+
+        transform.position=newpos;
+    }
+}
